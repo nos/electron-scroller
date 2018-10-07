cc728110a8000f160aee0857beadf9077efd44ca
Matt Huggins <matt.huggins@gmail.com>
2018-10-07 12:38:56 -0500

    Support selector or element

--- index.js
+++ index.js
@@ -10,30 +10,30 @@ function Scroller() {
 }
 
 // Connect scroller to a webview
-// TODO: Allow array of selectors, allow element or array of elements
+// TODO: Allow array of selectors or array of elements
 Scroller.prototype.add = function (selector) {
   let self = this
   selector = selector || 'webview'
-  let webview = document.querySelector(selector)
+  let webview = isElement(selector) ? selector : document.querySelector(selector)
   if (webview === null || !isElement(webview)) {
     let msg = `Unable to find <webview> with selector = "${selector}".`
     throw new ReferenceError(msg)
   }
-  if (this.trackers.get(selector) !== undefined) {
+  if (this.trackers.get(webview) !== undefined) {
     let msg = `A webview with the selector "${selector}"`
     msg += ` is already being tracked`
     throw new DuplicateError(msg)
   }
-  let tracker = new ScrollTracker(selector)
+  let tracker = new ScrollTracker(webview)
 
   // Set up webview event listeners
   webview.addEventListener('ipc-message', function(e) {
     if (e.channel === EVENTS.DID_SCROLL) {
-      self.didScroll.call(self, selector, e.args[0].position)
+      self.didScroll.call(self, webview, e.args[0].position)
     }
   })
   webview.addEventListener('did-navigate', function() {
-    self.didNavigate.call(self, selector)
+    self.didNavigate.call(self, webview)
   })
   //webview.addEventListener('did-navigate-in-page', function() {
   //  Currently unused
@@ -65,11 +65,11 @@ Scroller.prototype.add = function (selector) {
     tracker.webviewReload.call(webview)
     self.motion = NAVTYPE.RELOAD
   }
-  this.trackers.set(selector, tracker)
+  this.trackers.set(webview, tracker)
 
   try {
     if (webview.getURL()) {
-      this.didNavigate(selector)
+      this.didNavigate(webview)
     }
   } catch (e) {
       // Nothing to do here. The webview isn't part of the DOM yet and will
@@ -80,14 +80,12 @@ Scroller.prototype.add = function (selector) {
 //
 // Event Handlers
 //
-Scroller.prototype.didScroll = function (selector, position) {
-  this.trackers.get(selector).didScroll(position)
+Scroller.prototype.didScroll = function (webview, position) {
+  this.trackers.get(webview).didScroll(position)
 }
 
-Scroller.prototype.didNavigate = function (selector) {
-  let webview = document.querySelector(selector)
-  let tracker = this.trackers.get(selector)
-  tracker.didNavigate(webview.getURL())
+Scroller.prototype.didNavigate = function (webview) {
+  this.trackers.get(webview).didNavigate(webview.getURL())
 }
 
 //
